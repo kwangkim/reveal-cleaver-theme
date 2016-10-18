1565aef7a650a35e0592a2e681252f062bae5110
kwangkim <kkim@qcc.cuny.edu>
2016-10-18 02:54:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 Reveal.initialize({
-        controls: true,
-        progress: true,
         history: true,
-        center: true,
         theme: 'default',
-        transition: 'default',
+        transition: 'linear',
         math: {
                 mathjax: 'https://cdn.mathjax.org/mathjax/latest/MathJax.js',
-                config: 'TeX-AMS_HTML-full'  // See http://docs.mathjax.org/en/latest/config-files.html
+                config: 'TeX-AMS_HTML-full' 
         },
         dependencies: [
                 { src: 'https://cdn.jsdelivr.net/reveal.js/3.0.0/plugin/math/math.js', async: true }
